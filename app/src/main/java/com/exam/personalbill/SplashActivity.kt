91f457891803543d3eea051ddf2a640af27c4e06
MengBowen-328p <mbw.mega328p@gmail.com>
2024-05-18 11:16:04 +0800

    Initial commit

---
+++ app/src/main/java/com/exam/personalbill/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.exam.personalbill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.content.Intent
+
+class SplashActivity : AppCompatActivity() {
+    private val splash_time: Long = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },splash_time)
+    }
+}
