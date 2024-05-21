9422d0be16e0724410f50f670c18658ac96bc869
Nicolas Meng <mbw.328p@gmail.com>
2024-05-21 12:33:21 +0800

    Update ListView

--- app/src/main/java/com/exam/personalbill/adapter/HomeAdapter.kt
+++ app/src/main/java/com/exam/personalbill/adapter/HomeAdapter.kt
@@ -1,34 +1,34 @@
 package com.exam.personalbill.adapter
-
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-import androidx.drawerlayout.R
-
-class HomeAdapter(context: Context, private val resource: Int, private val items: List<String>) :
-    ArrayAdapter<String>(context, resource, items) {
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(resource, parent, false)
-
-        val viewHolder: ViewHolder
-        if (view.tag == null) {
-            viewHolder = ViewHolder(view)
-            view.tag = viewHolder
-        } else {
-            viewHolder = view.tag as ViewHolder
-        }
-
-        viewHolder.listView.adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, items)
-
-        return view
-    }
-
-    class ViewHolder(view: View) {
-        val listView: ListView = view.findViewById(R.id.menu)
-    }
-}
+//
+//import android.content.Context
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.ArrayAdapter001
+//import android.widget.ListView
+//import android.widget.TextView
+//import androidx.drawerlayout.R
+//
+//class HomeAdapter(context: Context, private val resource: Int, private val items: List<String>) :
+//    ArrayAdapter<String>(context, resource, items) {
+//
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//        val view = convertView ?: LayoutInflater.from(context).inflate(resource, parent, false)
+//
+//        val viewHolder: ViewHolder
+//        if (view.tag == null) {
+//            viewHolder = ViewHolder(view)
+//            view.tag = viewHolder
+//        } else {
+//            viewHolder = view.tag as ViewHolder
+//        }
+//
+//        viewHolder.listView.adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, items)
+//
+//        return view
+//    }
+//
+//    class ViewHolder(view: View) {
+//        val listView: ListView = view.findViewById(R)
+//    }
+//}
--- app/src/main/java/com/exam/personalbill/fragment/Home.kt
+++ app/src/main/java/com/exam/personalbill/fragment/Home.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import com.exam.personalbill.Database
 import com.exam.personalbill.R
+import android.widget.ArrayAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class Home : Fragment() {
 
+    private lateinit var listView: ListView
+
     private lateinit var db: Database
 
     // TODO: Rename and change types of parameters
@@ -32,12 +36,15 @@ class Home : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        val view = inflater.inflate(R.layout.fragment_home,container,false)
+//        listView = view.findViewById(R.id.menu_list)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
