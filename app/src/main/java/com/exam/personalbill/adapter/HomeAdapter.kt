05d03962528746d4b276d91f1ae7374a7007fb2f
Nicolas Meng <mbw.328p@gmail.com>
2024-05-21 14:19:15 +0800

    Update ListView

--- app/src/main/java/com/exam/personalbill/adapter/HomeAdapter.kt
+++ app/src/main/java/com/exam/personalbill/adapter/HomeAdapter.kt
@@ -1,34 +1,26 @@
 package com.exam.personalbill.adapter
-//
-//import android.content.Context
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.ArrayAdapter001
-//import android.widget.ListView
-//import android.widget.TextView
-//import androidx.drawerlayout.R
-//
-//class HomeAdapter(context: Context, private val resource: Int, private val items: List<String>) :
-//    ArrayAdapter<String>(context, resource, items) {
-//
-//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-//        val view = convertView ?: LayoutInflater.from(context).inflate(resource, parent, false)
-//
-//        val viewHolder: ViewHolder
-//        if (view.tag == null) {
-//            viewHolder = ViewHolder(view)
-//            view.tag = viewHolder
-//        } else {
-//            viewHolder = view.tag as ViewHolder
-//        }
-//
-//        viewHolder.listView.adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, items)
-//
-//        return view
-//    }
-//
-//    class ViewHolder(view: View) {
-//        val listView: ListView = view.findViewById(R)
-//    }
-//}
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import com.exam.personalbill.R
+
+class HomeAdapter(context: Context, private val data: List<String>) :
+    ArrayAdapter<String>(context, R.layout.menu_item, data) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var itemView = convertView
+        if (itemView == null) {
+            itemView = LayoutInflater.from(context).inflate(R.layout.menu_item, parent, false)
+        }
+
+        val textViewCustom = itemView!!.findViewById<TextView>(R.id.listview_textview)
+        textViewCustom.text = data[position]
+
+        return itemView
+    }
+}
